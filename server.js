7c7055a8eecfad7189d3fa8de6233858774bc0cb
Katherine Connolly <katerine.d.connolly@gmail.com>
2018-12-13 12:04:47 -0700

    layout setup without functionality

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000; // default port 3000
+var moment = require('moment');
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+// Main page
+app.get('/', (req, res) => {
+  res.render('main.ejs');
+});
+
+// Main page
+app.get('/gridwatch', (req, res) => {
+  res.render('home-page.ejs');
+});
+
+// Error handler
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res
+      .status(500)
+      .send('Oops! Something isn\'t right here! Please go back to the previous page');
+});
+
+// MOMENT JS
+
+
+// Port listener
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
