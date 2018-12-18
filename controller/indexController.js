11953120737893bc22fd25642c4b8a9282ea10ee
Alex Buck <buckatech@gmail.com>
2018-12-17 17:38:02 -0700

    add react schema

--- controller/indexController.js
+++ controller/indexController.js
@@ -1,6 +1,7 @@
 const db = require('../server/server');
 const to = require('../global/helpers').to;
 const svgFormatter = require('./helpers/indexHelper').svgFormatter;
+const sendRecentDataToServer = require('../server/lib/reactSchema').sendRecentDataToServer;
 /**
  * Pulls data from the server, adds a sum to the data object.
  * Then display the data as JSON
@@ -8,7 +9,7 @@ const svgFormatter = require('./helpers/indexHelper').svgFormatter;
  * @param {object} res response header
  */
 exports.render_homepage = async (req, res) => {
-  [err, dbData] = await to(db.getMostRecentData().then(function(data) {
+  [err, dbData] = await to(sendRecentDataToServer().then(function(data) {
     const outData = data;
     res.json({express: JSON.stringify(outData)});
   }));
--- server/lib/reactSchema.js
+++ server/lib/reactSchema.js
@@ -1,15 +1,62 @@
-const db = require('../server')
+const db = require('../server');
 
-db.getMostRecentData().then((out) => {
-  console.log(breakDownContainerSchema(out));
-})
+function sort(obj) {
+  return Object.keys(obj).sort(function(a, b) {
+    return obj[b] - obj[a];
+  });
+}
+
+const sortArrByMC = (arr) => {
+  arr.sort(function(a, b) {
+    return b.MC - a.MC;
+  });
+  return arr;
+};
+const returnSortedObject = (obj) => {
+  const json_result = breakDownContainerSchema(obj);
+  const sorted = sort(json_result);
+  const output = sorted.map(function(key) {
+    return {[key]: json_result[key]};
+  });
+  return output;
+};
 
 const breakDownContainerSchema = (inputData) => {
   const target = inputData[0].data['MATH']['SUMS']['percentObj'];
-  let gasSum = 0
-  target['GAS'].forEach(element => {
-    gasSum += Object.values(element)[0]
+  let gasSum = 0;
+  target['GAS'].forEach((element) => {
+    gasSum += Object.values(element)[0];
   });
-  target['GAS'] = gasSum
-  return target
-}
+  target['GAS'] = gasSum;
+  return target;
+};
+
+const plantBreakDownSchema = (inputData, targetVal) => {
+  targetVal.forEach((element) => {
+    delete element.DCR;
+  });
+  return targetVal;
+};
+
+db.getMostRecentData().then((out) => {
+  const targetValues = ['COAL', 'HYDRO', 'WIND', 'BIOMASS AND OTHER', 'GAS'];
+  const gasVals = ['Simple Cycle', 'Cogeneration', 'Combined Cycle'];
+  const outObj = {};
+  // output = returnSortedObject(out)
+  // console.log({"breakDownContainer": output})
+  targetValues.forEach((element) => {
+    if (element === 'GAS') {
+        for (let i = 0; i < gasVals.length; i++) {
+          //console.log(gasVals[i])
+          const target = out[0].data['GAS'][i][gasVals[i]];
+          outObj[gasVals[i]] = sortArrByMC(plantBreakDownSchema(out, target));
+        }
+    } else if (element !== 'GAS') {
+      const target = out[0].data[element];
+      outObj[element] = sortArrByMC(plantBreakDownSchema(out, target));
+    }
+  });
+  console.log(outObj)
+  // console.log(sortArrByMC(plantBreakDownSchema(out)))
+});
+
