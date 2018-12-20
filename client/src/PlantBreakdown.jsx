17e373445043a9dde0e04c4308498a2e65779a56
Taylor Cameron <taylorcameron@Taylors-MacBook-Air.local>
2018-12-20 14:15:39 -0700

    re-format state for plant assets

--- client/src/PlantBreakdown.jsx
+++ client/src/PlantBreakdown.jsx
@@ -1,12 +1,27 @@
 import React, {Component} from 'react';
 
+function getAssets(type, plants) {
+    const outputArr = [];
+    if (type.toLowerCase() === "gas") {
+        outputArr.push({ "subtitle": "cogeneration", "data": plants['Cogeneration']})
+        outputArr.push({ "subtitle": "Combined Cycle", "data": plants['Combined Cycle'] })
+        outputArr.push({ "subtitle": "Simple Cycle", "data": plants['Simple Cycle'] })
+        return outputArr; // this is an object
+    }
+    return [{"data": plants[type]}]; // this is an array
+}
 class PlantBreakdown extends Component {
 
     render() {
-        console.log(this.props)
-        const percentageObj = this.props.generation.breakdownContainer.find(type => type[this.props.energytype])
-        let percentage = percentageObj ? percentageObj[this.props.energytype] : 0;
+        const { energytype, generation, powerPlants } = this.props;
+        const percentageObj = generation.breakdownContainer.find(type => type[energytype])
+        let percentage = percentageObj ? percentageObj[energytype] : 0;
         percentage = (percentage * 100).toFixed(2)
+        const plants = powerPlants.plantBreakdown
+        const assets = getAssets(energytype, plants);
+        console.log("energytype on plant", energytype)
+        console.log("this is for you", plants);
+        console.log(assets)
         return(
             <div className="content-wrapper">
                 <div className="plant-container">
@@ -14,9 +29,9 @@ class PlantBreakdown extends Component {
                     <div className="power-plant-breakdown-table-header">
                         <div className="breakdown-table-title">GENERATION - PLANT
                         </div>
-                        <div className={`power-plant-breakdown-summary biofuel-bar ${this.props.energytype.toLowerCase()}-bar`}>
+                        <div className={`power-plant-breakdown-summary biofuel-bar ${energytype.toLowerCase()}-bar`}>
                         <div>
-                                <div className="power-plant-breakdown-summary-title">{this.props.energytype}</div>
+                                <div className="power-plant-breakdown-summary-title">{energytype}</div>
                         <div className="power-plant-breakdown-summary-bar-background">
                                     <div className="power-plant-breakdown-summary-bar" style={{width: `${percentage}%`}}></div>
                                 </div>
@@ -24,7 +39,7 @@ class PlantBreakdown extends Component {
                                     <div>
                                             <span className="power-plant-breakdown-summary-percentage">{percentage}</span>% (of total grid)</div>
                                         <div>
-                                            <span className="power-plant-breakdown-summary-total">-- </span> MW
+                                            {/* <div className="power-plant-breakdown-summary-total">-- </div> MW */}
                                         </div>
                                     </div>
                                 </div>
