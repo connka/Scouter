cfc8752cadde8014a8c2a288af515aa87d6be735
Taylor Cameron <taylorcameron@Taylors-MacBook-Air.local>
2018-12-21 15:13:59 -0700

    modal displaying powerplant info added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,65 +2,78 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './Header';
 import SummaryContainer from './SummaryContainer';
-import BreakdownContainer from './BreakDownContainer.jsx';
-import PlantBreakdown from './PlantBreakdown.jsx';
+import BreakdownContainer from './BreakDownContainer';
+import PlantBreakdown from './PlantBreakdown';
 import Legend from './Legend.jsx';
 import SunburstGraph from './SunburstGraph';
 import Disclaimer from './Disclaimer';
+import Modal from './Modal';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      energytype: 'GAS'
+      energytype: "GAS"
     };
   }
   // When component mounts call the Api and send response to the client
   componentDidMount() {
     this.callApi()
-      .then(res => this.setState((function(){
-        return res.express
-      })))
+      .then(res =>
+        this.setState(function() {
+          return res.express;
+        })
+      )
       .catch(err => console.log(err));
   }
   // Fetch data from the backend served at localhost:5000
-  async callApi () {
-    const response = await fetch('http://localhost:5000/api/hello');
+  async callApi() {
+    const response = await fetch("http://localhost:5000/api/hello");
     const body = await response.json();
     if (response.status !== 200) throw Error(body.message);
 
     return body;
-  };
+  }
 
-  handleClick = (event) => {
+  handleClick = event => {
     this.setState({
       energytype: event.currentTarget.id
     });
-  }
+  };
 
+  setModal = modalData => this.setState({ modalData });
+
+  closeModal = () => this.setState({ modalData: undefined })
   render() {
-
-    if (!this.state[0]) return (
-      <p>{this.state.response}</p>
-    );
+    console.log(this.state);
+    if (!this.state[0]) return <p>{this.state.response}</p>;
     return (
       <div>
         <div>
-          <Header date={this.state[0]}/>
+          <Header date={this.state[0]} />
         </div>
-        <div className='content-wrapper'>
-          <SummaryContainer summary={this.state[1]}/>
-          <BreakdownContainer generation={this.state[2]} button={this.handleClick}/>
-          <PlantBreakdown powerPlants={this.state[3]} generation={this.state[2]} energytype={this.state.energytype}/>
+        <div className="content-wrapper">
+          <SummaryContainer summary={this.state[1]} />
+          <BreakdownContainer
+            generation={this.state[2]}
+            button={this.handleClick}
+          />
+          <PlantBreakdown
+            powerPlants={this.state[3]}
+            generation={this.state[2]}
+            energytype={this.state.energytype}
+            setModalData={this.setModal}
+          />
         </div>
-        <div className='second-row'>
+        <div className="second-row">
           <SunburstGraph />
           <Legend />
         </div>
         <Disclaimer />
+        <Modal data={this.state.modalData} closeModal={this.closeModal} />
       </div>
     );
-}
+  }
 }
 
 
