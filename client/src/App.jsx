387ee7cd9c36001da41746dd87a2bc413ba2aa9e
Taylor Cameron <taylorcameron@Taylors-MacBook-Air.local>
2019-01-09 09:33:24 -0700

    return json from fetch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -35,7 +35,10 @@ class App extends Component {
   async callApi() {
     const response = await fetch("https://scouterscrape.azurewebsites.net/api/RequestScrapeData?code=Fv2vV9aGCvlI2FK9/Dtv5CUPJlYUlrKGvYddEABqO/2baT5Eie7w3w==")
                            .then(function(res){
-                             console.log(res.json())
+                             res.json().then((value) => {
+                               console.log(value)
+                               return value
+                             })
                            });
     // const jsonResponse = await response.json();
     const body = sortIncoming(response);
